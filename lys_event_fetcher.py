c4eeaaa9acdf7bf7f97ac421a39d57e025148f89
Corentin Dautreme <co@fedora.home>
2020-01-26 14:46:05 +0100

    Improve event filtering and adding date context to logs output

--- lys_event_fetcher.py
+++ lys_event_fetcher.py
@@ -100,7 +100,7 @@ class EventSuggestion():
 		yield 'country', self.country
 		yield 'name', self.name
 		yield 'stage', self.stage
-		yield 'dateTimesCet', self.dateTimesCet
+		yield 'dateTimesCet', list(map(lambda d: d['date'], self.dateTimesCet))
 		yield 'sourceLink', self.sourceLink
 		yield 'accepted', self.accepted
 		yield 'processed', self.processed
@@ -189,19 +189,19 @@ def check_for_repetition_expression(sentence):
 				it_date = begin_date
 				i = 1
 				while it_date < end_date:
-					events.append(EventSuggestion("", "", "Night " + str(i), [it_date.strftime("%Y-%m-%d") + "T20:00:00"], ""))
+					events.append(EventSuggestion("", "", "Night " + str(i), [{'date': it_date.strftime("%Y-%m-%d") + "T20:00:00", 'context': ''}], ""))
 					it_date += datetime.timedelta(days=7)
 					i += 1
-				events.append(EventSuggestion("", "", "Final", [it_date.strftime("%Y-%m-%d") + "T20:00:00"], ""))
+				events.append(EventSuggestion("", "", "Final", [{'date': it_date.strftime("%Y-%m-%d") + "T20:00:00", 'context': ''}], ""))
 				return events
 			elif frequency in ["day", "night", "evening"]:
 				it_date = begin_date
 				i = 1
 				while it_date < end_date:
-					events.append(EventSuggestion("", "", "Night " + str(i), [it_date.strftime("%Y-%m-%d") + "T20:00:00"], ""))
+					events.append(EventSuggestion("", "", "Night " + str(i), [{'date': it_date.strftime("%Y-%m-%d") + "T20:00:00", 'context': ''}], ""))
 					it_date += datetime.timedelta(days=1)
 					i += 1
-				events.append(EventSuggestion("", "", "Final", [it_date.strftime("%Y-%m-%d") + "T20:00:00"], ""))
+				events.append(EventSuggestion("", "", "Final", [{'date': it_date.strftime("%Y-%m-%d") + "T20:00:00", 'context': ''}], ""))
 				return events
 		except Exception as e:
 			print("Error parsing repetition expression \"" + sentence + "\" against pattern \"" + start_every_end_pattern + "\" - Exception is: " + str(e))
@@ -243,19 +243,19 @@ def check_for_repetition_expression(sentence):
 				it_date = begin_date
 				i = 1
 				while it_date < end_date:
-					events.append(EventSuggestion("", "", "Night " + str(i), [it_date.strftime("%Y-%m-%d") + "T20:00:00"], ""))
+					events.append(EventSuggestion("", "", "Night " + str(i), [{'date': it_date.strftime("%Y-%m-%d") + "T20:00:00", 'context': ''}], ""))
 					it_date += datetime.timedelta(days=7)
 					i += 1
-				events.append(EventSuggestion("", "", "Final", [it_date.strftime("%Y-%m-%d") + "T20:00:00"], ""))
+				events.append(EventSuggestion("", "", "Final", [{'date': it_date.strftime("%Y-%m-%d") + "T20:00:00", 'context': ''}], ""))
 				return events
 			elif frequency in ["day", "night", "evening"]:
 				it_date = begin_date
 				i = 1
 				while it_date < end_date:
-					events.append(EventSuggestion("", "", "Night " + str(i), [it_date.strftime("%Y-%m-%d") + "T20:00:00"], ""))
+					events.append(EventSuggestion("", "", "Night " + str(i), [{'date': it_date.strftime("%Y-%m-%d") + "T20:00:00", 'context': ''}], ""))
 					it_date += datetime.timedelta(days=1)
 					i += 1
-				events.append(EventSuggestion("", "", "Final", [it_date.strftime("%Y-%m-%d") + "T20:00:00"], ""))
+				events.append(EventSuggestion("", "", "Final", [{'date': it_date.strftime("%Y-%m-%d") + "T20:00:00", 'context': ''}], ""))
 				return events
 		except Exception as e:
 			print("Error parsing repetition expression \"" + sentence + "\" against pattern \"" + every_from_to_pattern + "\" - Exception is: " + str(e))
@@ -296,19 +296,19 @@ def check_for_repetition_expression(sentence):
 				it_date = begin_date
 				i = 1
 				while it_date < end_date:
-					events.append(EventSuggestion("", "", "Night " + str(i), [it_date.strftime("%Y-%m-%d") + "T20:00:00"], ""))
+					events.append(EventSuggestion("", "", "Night " + str(i), [{'date': it_date.strftime("%Y-%m-%d") + "T20:00:00", 'context': ''}], ""))
 					it_date += datetime.timedelta(days=7)
 					i += 1
-				events.append(EventSuggestion("", "", "Final", [it_date.strftime("%Y-%m-%d") + "T20:00:00"], ""))
+				events.append(EventSuggestion("", "", "Final", [{'date': it_date.strftime("%Y-%m-%d") + "T20:00:00", 'context': ''}], ""))
 				return events
 			elif frequency in ["day", "night", "evening"]:
 				it_date = begin_date
 				i = 1
 				while it_date < end_date:
-					events.append(EventSuggestion("", "", "Night " + str(i), [it_date.strftime("%Y-%m-%d") + "T20:00:00"], ""))
+					events.append(EventSuggestion("", "", "Night " + str(i), [{'date': it_date.strftime("%Y-%m-%d") + "T20:00:00", 'context': ''}], ""))
 					it_date += datetime.timedelta(days=1)
 					i += 1
-				events.append(EventSuggestion("", "", "Final", [it_date.strftime("%Y-%m-%d") + "T20:00:00"], ""))
+				events.append(EventSuggestion("", "", "Final", [{'date': it_date.strftime("%Y-%m-%d") + "T20:00:00", 'context': ''}], ""))
 				return events
 		except Exception as e:
 			print("Error parsing repetition expression \"" + sentence + "\" against pattern \"" + from_to_every_pattern + "\" - Exception is: " + str(e))
@@ -320,9 +320,9 @@ def mark_event_suggestion_for_saving(suggested_event):
 	global NEXT_SUGGESTED_EVENT_ID
 
 	# remove dates for which an event with that name already exists in list
-	suggested_event.dateTimesCet = list(filter(lambda date: not any(e['dateTimeCet'][0:10] == date[0:10] and e['name'] == suggested_event.name for e in events), suggested_event.dateTimesCet))
+	suggested_event.dateTimesCet = [date for date in suggested_event.dateTimesCet if not any(e['dateTimeCet'][0:10] == date['date'][0:10] and e['name'] == suggested_event.name for e in events)]
 	# remove dates for which an event suggestion for that NF was already saved
-	suggested_event.dateTimesCet = list(filter(lambda date: not any(date[0:10] in list(map(lambda date: date[0:10], e['dateTimesCet'])) and e['name'] == suggested_event.name for e in suggested_events), suggested_event.dateTimesCet))
+	suggested_event.dateTimesCet = [date for date in suggested_event.dateTimesCet if not any(date['date'][0:10] in list(map(lambda d: d[0:10], e['dateTimesCet'])) and e['name'] == suggested_event.name for e in suggested_events)]
 
 	if len(suggested_event.dateTimesCet) > 0:
 		suggested_event.id = NEXT_SUGGESTED_EVENT_ID
@@ -347,7 +347,7 @@ def extract_events(event, is_local_env):
 
 	NEXT_SUGGESTED_EVENT_ID = 0
 	try:
-		NEXT_SUGGESTED_EVENT_ID = max(e['id'] for e in suggested_events)
+		NEXT_SUGGESTED_EVENT_ID = max(e['id'] for e in suggested_events) + 1
 	except ValueError:
 		pass
 
@@ -357,9 +357,13 @@ def extract_events(event, is_local_env):
 
 	items = root.find('channel').findall('item')
 
-	nf_items = filter(lambda item: ('National Selection' in list(map(lambda c: c.text, item.findall('category')))), items)
-	stories = []
+	nf_items = list(filter(lambda item: ('NATIONAL SELECTION' in list(map(lambda c: c.text.upper(), item.findall('category')))), items))
 
+	excluded_categories = [".*JUNIOR.*", ".*YOUNG MUSICIANS.*", ".*CHOIR.*", ".*YOUNG DANCERS.*"]
+	combined = "(" + ")|(".join(excluded_categories) + ")"
+	nf_items = [item for item in nf_items if not any(re.match(combined, category) for category in list(map(lambda c: c.text.upper(), item.findall('category'))))]
+
+	stories = []
 	event_suggestions = []
 
 	for item in nf_items:
@@ -394,21 +398,26 @@ def extract_events(event, is_local_env):
 
 				sentence = re.sub(re.compile('(January|February|March|April|May|June|July|August|September|October|November|December) ([0-9]+(?:st|nd|rd|th)*) and ([0-9]+(?:st|nd|rd|th)*)'), r'\1 \2, \1 \3,', sentence)
 				sentence = re.sub(re.compile('([0-9]+(?:st|nd|rd|th)*) and ([0-9]+(?:st|nd|rd|th)*) (January|February|March|April|May|June|July|August|September|October|November|December)'), r'\1 \3, \2 \3,', sentence)
-				found_dates = search_dates(sentence, languages=['en']) or []
+				found_dates = search_dates(sentence, languages=['en'], settings={'RETURN_AS_TIMEZONE_AWARE': False}) or []
 
 				# FIltering out the false positives
 				found_dates = list(filter(lambda d: re.match(re.compile("[a-z ]*20[0-9]{2}[a-z ]*"), d[0]) == None, found_dates))
 				found_dates = list(filter(lambda d: not(re.match(re.compile("^[a-zA-Z ]+$"), d[0]) != None and d[1].day == datetime.datetime.now().day), found_dates))
 				found_dates = list(filter(lambda d: d[1] > datetime.datetime.now(), found_dates))
 				found_dates = list(filter(lambda d: d[1].year <= datetime.datetime.now().year + 2, found_dates))
-				dates.extend(list(map(lambda d: d[1].strftime("%Y-%m-%d") + "T20:00:00", found_dates)))
+				dates.extend(list(map(lambda d: {'date': d[1].strftime("%Y-%m-%d") + "T20:00:00", 'context': d[0]}, found_dates)))
 
-		dates = sorted(list(set(dates)))
+		filtered_dates = []
+		for date in dates:
+			if not any(d['date'] == date['date'] for d in filtered_dates):
+				filtered_dates.append(date)
+		filtered_dates = sorted(filtered_dates, key=lambda d: d['date'])
 
 		if len(events_for_story) == 0:
-			for i in range(1,len(dates)+1):
-				date = dates[i-1]
-				events_for_story.append(EventSuggestion(story.country, get_event_for_country(story.country), ("Semi-final " if has_semi_finals else "Night ") + str(i) if i < len(dates) else "Final", [date], story.sourceLink))
+			for i in range(1,len(filtered_dates)+1):
+				date = filtered_dates[i-1]
+				event_suggestion = EventSuggestion(story.country, get_event_for_country(story.country), ("Semi-final " if has_semi_finals else "Night ") + str(i) if i < len(filtered_dates) else "Final", [date], story.sourceLink)
+				events_for_story.append(event_suggestion)
 
 		event_suggestions.extend(events_for_story)
 
@@ -419,7 +428,7 @@ def extract_events(event, is_local_env):
 
 	print("\nSaved event suggestions:")
 	for event in event_suggestions_to_be_saved:
-		print(event)
+		print(event.__str__())
 		if(not IS_TEST):
 			try:
 				suggested_events_table.put_item(Item=dict(event))
