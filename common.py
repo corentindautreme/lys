6c037d998011717918368c739fa12f07c716971f
Corentin Dautreme <co@fedora.home>
2021-11-20 13:54:28 +0100

    Adding dump lambda

--- common.py
+++ common.py
@@ -1,4 +1,7 @@
 import tweepy
+import datetime
+from operator import itemgetter as i
+from functools import cmp_to_key
 
 DATETIME_CET_FORMAT = "%Y-%m-%dT%H:%M:%S"
 
@@ -70,3 +73,39 @@ def create_tweepy_api(consumer_key, consumer_secret, access_token, access_token_
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
     return tweepy.API(auth)
+
+def get_current_season_range_for_date(date):
+    if date.month > 3:
+        season_start = datetime.datetime(date.year, 9, 1, 0, 0, 0)
+        season_end = datetime.datetime(date.year + 1, 3, 31, 23, 59, 59)
+        return (season_start, season_end)
+    elif date.month >= 1:
+        season_start = datetime.datetime(date.year - 1, 9, 1, 0, 0, 0)
+        season_end = datetime.datetime(date.year, 3, 31, 23, 59, 59)
+        return (season_start, season_end)
+
+def cmp(x, y):
+    """
+    Replacement for built-in function cmp that was removed in Python 3
+
+    Compare the two objects x and y and return an integer according to
+    the outcome. The return value is negative if x < y, zero if x == y
+    and strictly positive if x > y.
+
+    https://portingguide.readthedocs.io/en/latest/comparisons.html#the-cmp-function
+    """
+
+    return (x > y) - (x < y)
+
+def multikeysort(items, columns):
+    comparers = [
+        ((i(col[1:].strip()), -1) if col.startswith('-') else (i(col.strip()), 1))
+        for col in columns
+    ]
+    def comparer(left, right):
+        comparer_iter = (
+            cmp(fn(left), fn(right)) * mult
+            for fn, mult in comparers
+        )
+        return next((result for result in comparer_iter if result), 0)
+    return sorted(items, key=cmp_to_key(comparer))
---
+++ lys_dump.py
@@ -0,0 +1,97 @@
+import datetime
+import requests
+import json
+import os
+
+try:
+    import boto3
+    from boto3.dynamodb.conditions import Key
+except ImportError:
+    pass
+
+from urllib3.exceptions import InsecureRequestWarning
+
+from common import get_current_season_range_for_date, DATETIME_CET_FORMAT, multikeysort
+
+requests.packages.urllib3.disable_warnings(category=InsecureRequestWarning)
+
+def dump_calendar_to_github():
+    GITHUB_API_BASE = "https://api.github.com"
+
+    username = os.environ['GITHUB_USERNAME']
+    pat = os.environ['GITHUB_PAT']
+
+    auth_header = base64.b64encode(str.encode(username + ":" + pat)).decode("ascii")
+
+    with open('lys_dump.json', 'r') as f:
+        dump = f.read()
+
+    response = requests.get(GITHUB_API_BASE + "/repos/lyseurovision/lys.github.io/branches/main", headers={'Authorization': 'Basic ' + auth_header}, verify=False)
+    last_commit_sha = response.json()['commit']['sha']
+
+    response = requests.post(GITHUB_API_BASE + "/repos/lyseurovision/lys.github.io/git/blobs", headers={'Authorization': 'Basic ' + auth_header}, json={'content': dump, 'encoding': 'utf-8'}, verify=False)
+    utf8_blob_sha = response.json()['sha']
+
+    data = {
+        'base_tree': last_commit_sha,
+        'tree': [
+            {
+                'path': 'lys_dump.json',
+                'mode': '100644',
+                'type': 'blob',
+                'sha': utf8_blob_sha
+            }
+        ]
+    }
+    response = requests.post(GITHUB_API_BASE + "/repos/lyseurovision/lys.github.io/git/trees", headers={'Authorization': 'Basic ' + auth_header}, json=data, verify=False)
+    tree_sha = response.json()['sha']
+
+    data = {
+        'message': 'Lys calendar dump',
+        'author': {
+            'name': 'Lys dump lambda',
+            'email': 'noreply@lys.githib.io'
+        },
+        'parents': [
+            last_commit_sha
+        ],
+        'tree': tree_sha
+    }
+    response = requests.post(GITHUB_API_BASE + "/repos/lyseurovision/lys.github.io/git/commits", headers={'Authorization': 'Basic ' + auth_header}, json=data, verify=False)
+    new_commit_sha = response.json()['sha']
+
+    data = {
+        'ref': 'refs/heads/main',
+        'sha': new_commit_sha
+    }
+    response = requests.post(GITHUB_API_BASE + "/repos/lyseurovision/lys.github.io/git/refs/heads/main", headers={'Authorization': 'Basic ' + auth_header}, json=data, verify=False)
+
+
+def main(event, context):
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table('lys_events')
+
+    today = datetime.datetime.now()
+    (season_start, season_end) = get_current_season_range_for_date(today)
+
+    events = table.scan(
+        FilterExpression=Key('dateTimeCet').between(season_start.strftime(DATETIME_CET_FORMAT), season_end.strftime(DATETIME_CET_FORMAT))
+    )['Items']
+    sorted_events = multikeysort(events, ['dateTimeCet', 'country'])
+
+    latest_dump = requests.get('https://raw.githubusercontent.com/LysEurovision/lys.github.io/main/lys_dump.json', verify=False).json()
+    
+    if json.dumps(latest_dump) == json.dumps(sorted_events):
+        print("No diff between latest dump and current event list - nothing to do")
+        return "No diff between latest dump and current event list - nothing to do"
+
+    with open('lys_dump.json', 'w') as f:
+        f.write(json.dumps(sorted_events))
+
+    dump_calendar_to_github()
+        print("Dumped calendar to Github")
+        return "Dumped calendar to Github"
+
+
+if __name__ == '__main__':
+    main(event={}, context={})
