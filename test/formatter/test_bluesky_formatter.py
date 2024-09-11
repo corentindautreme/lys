e5e86600f1633c20c602fbd8565658e78a7e2f63
corentindautreme <10880524+corentindautreme@users.noreply.github.com>
2024-09-11 15:03:23 +0200

    WIP Refactoring

--- test/formatter/test_bluesky_formatter.py
+++ test/formatter/test_bluesky_formatter.py
@@ -4,7 +4,7 @@ import datetime
 import requests
 
 from bs4 import BeautifulSoup
-from unittest.mock import patch
+from unittest.mock import patch, MagicMock
 
 from formatter.bluesky_formatter import BlueskyFormatter
 
@@ -49,7 +49,8 @@ class BlueskyFormatterTest(unittest.TestCase):
         ]
         post_string = "TODAY | \U0001F1F8\U0001F1EA SWEDEN\n---------\n\U0001F4FC Melodifestivalen\n\U0001F3C6 Heat 2\n\U0001F553 20:00 CET\n---------\n\U0001F4FA https://svtplay.se."
         
-        with patch.object(BeautifulSoup, 'find', side_effect=[{"content": "MOCK_TITLE"}, {"content": "MOCK_DESCR"}]) as mock_bs, patch.object(requests, 'get', return_value={"status_code": 200}) as mock_req:
+        mock_requests_resp = MagicMock(text="<>")
+        with patch.object(BeautifulSoup, 'find', side_effect=[{"content": "MOCK_TITLE"}, {"content": "MOCK_DESCR"}]) as mock_bs, patch.object(requests, 'get', return_value=mock_requests_resp) as mock_req:
             post = self.formatter.format_post(post_string, events)
 
         self.assertTrue(type(post) is dict)
