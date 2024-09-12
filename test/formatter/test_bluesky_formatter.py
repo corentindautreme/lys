e0d8b87ee7b2961c433fc05ee7a430968648419f
corentindautreme <10880524+corentindautreme@users.noreply.github.com>
2024-09-12 09:37:21 +0200

    WIP Refactoring

--- test/formatter/test_bluesky_formatter.py
+++ test/formatter/test_bluesky_formatter.py
@@ -83,3 +83,52 @@ class BlueskyFormatterTest(unittest.TestCase):
         self.assertEqual(post['embed']['external']['description'], "MOCK_DESCRIPTION")
 
         self.formatter.include_link_card = False
+
+
+def test_when_formatting_post_with_multiple_links_and_link_card_disabled_should_generate_post_with_facets(self):
+    events = [
+        {'country': 'Sweden', 'name': 'Melodifestivalen', 'stage': 'Final', 'dateTimeCet': '2021-03-13T20:00:00', 'watchLinks': [{'link': 'https://svtplay.se', 'comment': 'Recommended link', 'live': 1}, {'link': 'https://svtplay.se', 'comment': 'Another link', 'live': 1}]},
+        {'country': 'Norway', 'name': 'Melodi Grand Prix', 'stage': 'Final', 'dateTimeCet': '2021-03-13T20:00:00', 'watchLinks': [{'link': 'https://tv.nrk.no', 'comment': 'Recommended link', 'live': 1}]}
+    ]
+    post_string = "\U0001F6A8 5 MINUTES REMINDER! (thread \U00002B07\U0000FE0F)\n---------\n\U0001F1F8\U0001F1EA Melodifestivalen - Final (https://svtplay.se OR https://svtplay.se (Another link))\n---------\n\U0001F1F3\U0001F1F4 Melodi Grand Prix - Final (https://tv.nrk.no)"
+
+    post = self.formatter.format_post(post_string, events)
+
+    self.assertTrue(type(post) is dict)
+    self.assertEqual(post['text'], post_string)
+    self.assertEqual(post['$type'], "app.bsky.feed.post")
+    self.assertEqual(post['langs'], ["en-US"])
+    self.assertTrue(self.ISO_TIMESTAMP_PATTERN.match(post['createdAt']))
+    post_datetime = datetime.datetime.fromisoformat(post['createdAt'].replace("Z", "+00:00"))
+    # verify that the attached datetime is in the past (and reasonably close to now)
+    self.assertTrue(self.is_dt_not_older_than(post_datetime, seconds=2))
+
+    # check that there is no card
+    self.assertTrue("embed" not in post)
+
+    self.assertTrue(type(post['facets']) is list)
+    self.assertEqual(len(post['facets']), 3)
+
+    self.assertTrue(type(post['facets'][0]['index']) is dict)
+    self.assertTrue("byteStart" in post['facets'][0]['index'])
+    self.assertTrue("byteEnd" in post['facets'][0]['index'])
+    self.assertTrue(type(post['facets'][0]['features']) is list)
+    self.assertEqual(len(post['facets'][0]['features']), 1)
+    self.assertEqual(post['facets'][0]['features'][0]['$type'], "app.bsky.richtext.facet#link")
+    self.assertEqual(post['facets'][0]['features'][0]['uri'], "https://svtplay.se")
+
+    self.assertTrue(type(post['facets'][1]['index']) is dict)
+    self.assertTrue("byteStart" in post['facets'][1]['index'])
+    self.assertTrue("byteEnd" in post['facets'][1]['index'])
+    self.assertTrue(type(post['facets'][1]['features']) is list)
+    self.assertEqual(len(post['facets'][1]['features']), 1)
+    self.assertEqual(post['facets'][1]['features'][0]['$type'], "app.bsky.richtext.facet#link")
+    self.assertEqual(post['facets'][1]['features'][0]['uri'], "https://svtplay.se")
+
+    self.assertTrue(type(post['facets'][2]['index']) is dict)
+    self.assertTrue("byteStart" in post['facets'][2]['index'])
+    self.assertTrue("byteEnd" in post['facets'][2]['index'])
+    self.assertTrue(type(post['facets'][2]['features']) is list)
+    self.assertEqual(len(post['facets'][2]['features']), 1)
+    self.assertEqual(post['facets'][2]['features'][0]['$type'], "app.bsky.richtext.facet#link")
+    self.assertEqual(post['facets'][2]['features'][0]['uri'], "https://tv.nrk.no")
