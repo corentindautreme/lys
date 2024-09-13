574f891c1a9af33ba4202d89446ac752a8a6dc68
corentindautreme <10880524+corentindautreme@users.noreply.github.com>
2024-09-13 10:44:56 +0200

    WIP Refactoring

--- test/generator/test_weekly_generator.py
+++ test/generator/test_weekly_generator.py
@@ -12,17 +12,17 @@ class WeeklyGeneratorTest(unittest.TestCase):
         self.assertEqual(self.generator.is_single_post(None), True) 
         self.assertEqual(self.generator.is_single_post([]), True)
         events = [
-            {'country': 'Estonia', 'name': 'Eesti Laul', 'stage': 'Semi-final 1', 'dateTimeCet': '2021-02-02T20:00:00', 'watchLink': 'https://err.tv'},
-            {'country': 'Sweden', 'name': 'Melodifestivalen', 'stage': 'Heat 1', 'dateTimeCet': '2021-02-06T20:00:00', 'watchLink': 'https://svtplay.se'},
-            {'country': 'Norway', 'name': 'Melodi Grand Prix', 'stage': 'Heat 4', 'dateTimeCet': '2021-02-06T19:50:00', 'watchLink': 'https://nrk.no/mgp'}
+            {'country': 'Estonia', 'name': 'Eesti Laul', 'stage': 'Semi-final 1', 'dateTimeCet': '2021-02-02T20:00:00', 'watchLinks': [{'link': 'https://err.tv', 'comment': 'Recommended link', 'live': 1},
+            {'country': 'Sweden', 'name': 'Melodifestivalen', 'stage': 'Heat 1', 'dateTimeCet': '2021-02-06T20:00:00', 'watchLinks': [{'link': 'https://svtplay.se', 'comment': 'Recommended link', 'live': 1},
+            {'country': 'Norway', 'name': 'Melodi Grand Prix', 'stage': 'Heat 4', 'dateTimeCet': '2021-02-06T19:50:00', 'watchLinks': [{'link': 'https://nrk.no/mgp', 'comment': 'Recommended link', 'live': 1}
         ]
         self.assertEqual(self.generator.is_single_post(events), True)
-        events = [{'country': 'Sweden', 'name': 'Melodifestivalen', 'stage': 'Andra Chansen', 'dateTimeCet': '2021-03-06T20:00:00', 'watchLink': 'https://svtplay.se'}]
+        events = [{'country': 'Sweden', 'name': 'Melodifestivalen', 'stage': 'Andra Chansen', 'dateTimeCet': '2021-03-06T20:00:00', 'watchLinks': [{'link': 'https://svtplay.se', 'comment': 'Recommended link', 'live': 1}]
         self.assertEqual(self.generator.is_single_post(events), True)
 
 
     def test_when_one_event_then_post_should_only_contain_this_one_event(self):
-        events = [{'country': 'Sweden', 'name': 'Melodifestivalen', 'stage': 'Andra Chansen', 'dateTimeCet': '2021-03-06T20:00:00', 'watchLink': 'https://svtplay.se'}]
+        events = [{'country': 'Sweden', 'name': 'Melodifestivalen', 'stage': 'Andra Chansen', 'dateTimeCet': '2021-03-06T20:00:00', 'watchLinks': [{'link': 'https://svtplay.se', 'comment': 'Recommended link', 'live': 1}]
         thread = self.generator.generate_thread(events)
         self.assertEqual(len(thread), 1)
         self.assertEqual(thread[0], "\U0001F5D3 COMING UP NEXT WEEK:\n\n - Saturday 06: \U0001F1F8\U0001F1EA")
@@ -30,9 +30,9 @@ class WeeklyGeneratorTest(unittest.TestCase):
 
     def test_when_multiple_events_then_post_should_contain_them_all(self):
         events = [
-            {'country': 'Estonia', 'name': 'Eesti Laul', 'stage': 'Semi-final 1', 'dateTimeCet': '2021-02-02T20:00:00', 'watchLink': 'https://err.tv'},
-            {'country': 'Sweden', 'name': 'Melodifestivalen', 'stage': 'Heat 1', 'dateTimeCet': '2021-02-06T20:00:00', 'watchLink': 'https://svtplay.se'},
-            {'country': 'Norway', 'name': 'Melodi Grand Prix', 'stage': 'Heat 4', 'dateTimeCet': '2021-02-06T19:50:00', 'watchLink': 'https://nrk.no/mgp'}
+            {'country': 'Estonia', 'name': 'Eesti Laul', 'stage': 'Semi-final 1', 'dateTimeCet': '2021-02-02T20:00:00', 'watchLinks': [{'link': 'https://err.tv', 'comment': 'Recommended link', 'live': 1},
+            {'country': 'Sweden', 'name': 'Melodifestivalen', 'stage': 'Heat 1', 'dateTimeCet': '2021-02-06T20:00:00', 'watchLinks': [{'link': 'https://svtplay.se', 'comment': 'Recommended link', 'live': 1},
+            {'country': 'Norway', 'name': 'Melodi Grand Prix', 'stage': 'Heat 4', 'dateTimeCet': '2021-02-06T19:50:00', 'watchLinks': [{'link': 'https://nrk.no/mgp', 'comment': 'Recommended link', 'live': 1}
         ]
         thread = self.generator.generate_thread(events)
         self.assertEqual(len(thread), 1)
@@ -41,9 +41,9 @@ class WeeklyGeneratorTest(unittest.TestCase):
 
     def test_when_events_include_a_final_then_post_should_contain_final_indicators(self):
         events = [
-            {'country': 'Estonia', 'name': 'Eesti Laul', 'stage': 'Semi-final 1', 'dateTimeCet': '2021-02-02T20:00:00', 'watchLink': 'https://err.tv'},
-            {'country': 'Sweden', 'name': 'Melodifestivalen', 'stage': 'Heat 1', 'dateTimeCet': '2021-02-06T20:00:00', 'watchLink': 'https://svtplay.se'},
-            {'country': 'Norway', 'name': 'Melodi Grand Prix', 'stage': 'Final', 'dateTimeCet': '2021-02-06T19:50:00', 'watchLink': 'https://nrk.no/mgp'}
+            {'country': 'Estonia', 'name': 'Eesti Laul', 'stage': 'Semi-final 1', 'dateTimeCet': '2021-02-02T20:00:00', 'watchLinks': [{'link': 'https://err.tv', 'comment': 'Recommended link', 'live': 1},
+            {'country': 'Sweden', 'name': 'Melodifestivalen', 'stage': 'Heat 1', 'dateTimeCet': '2021-02-06T20:00:00', 'watchLinks': [{'link': 'https://svtplay.se', 'comment': 'Recommended link', 'live': 1},
+            {'country': 'Norway', 'name': 'Melodi Grand Prix', 'stage': 'Final', 'dateTimeCet': '2021-02-06T19:50:00', 'watchLinks': [{'link': 'https://nrk.no/mgp', 'comment': 'Recommended link', 'live': 1}
         ]
         thread = self.generator.generate_thread(events)
         self.assertEqual(len(thread), 1)
@@ -52,9 +52,9 @@ class WeeklyGeneratorTest(unittest.TestCase):
 
     def test_when_events_include_country_without_known_flag_should_replace_by_country_name(self):
         events = [
-            {'country': 'Estonia', 'name': 'Eesti Laul', 'stage': 'Semi-final 1', 'dateTimeCet': '2021-02-02T20:00:00', 'watchLink': 'https://err.tv'},
-            {'country': 'Sweden', 'name': 'Melodifestivalen', 'stage': 'Heat 1', 'dateTimeCet': '2021-02-06T20:00:00', 'watchLink': 'https://svtplay.se'},
-            {'country': 'FYR Macedonia', 'name': 'What is the Macedonian for "Song" again?', 'stage': 'Final', 'dateTimeCet': '2021-02-06T19:50:00', 'watchLink': 'https://google.it.mate'}
+            {'country': 'Estonia', 'name': 'Eesti Laul', 'stage': 'Semi-final 1', 'dateTimeCet': '2021-02-02T20:00:00', 'watchLinks': [{'link': 'https://err.tv', 'comment': 'Recommended link', 'live': 1},
+            {'country': 'Sweden', 'name': 'Melodifestivalen', 'stage': 'Heat 1', 'dateTimeCet': '2021-02-06T20:00:00', 'watchLinks': [{'link': 'https://svtplay.se', 'comment': 'Recommended link', 'live': 1},
+            {'country': 'FYR Macedonia', 'name': 'What is the Macedonian for "Song" again?', 'stage': 'Final', 'dateTimeCet': '2021-02-06T19:50:00', 'watchLinks': [{'link': 'https://google.it.mate', 'comment': 'Recommended link', 'live': 1}
         ]
         thread = self.generator.generate_thread(events)
         self.assertEqual(len(thread), 1)
@@ -63,9 +63,9 @@ class WeeklyGeneratorTest(unittest.TestCase):
 
     def test_when_events_include_multiple_events_for_one_country_the_country_should_only_appear_once_in_the_post(self):
         events = [
-            {'country': 'Sweden', 'name': 'Melodifestivalen', 'stage': 'Heat 1', 'dateTimeCet': '2021-02-06T20:00:00', 'watchLink': 'https://svtplay.se'},
-            {'country': 'Norway', 'name': 'Melodi Grand Prix', 'stage': 'Final', 'dateTimeCet': '2021-02-06T20:00:00', 'watchLink': 'https://nrk.no/mgp'},
-            {'country': 'Norway', 'name': 'Melodi Grand Prix', 'stage': 'Final (part 2)', 'dateTimeCet': '2021-02-06T21:20:00', 'watchLink': 'https://nrk.no/mgp'},
+            {'country': 'Sweden', 'name': 'Melodifestivalen', 'stage': 'Heat 1', 'dateTimeCet': '2021-02-06T20:00:00', 'watchLinks': [{'link': 'https://svtplay.se', 'comment': 'Recommended link', 'live': 1},
+            {'country': 'Norway', 'name': 'Melodi Grand Prix', 'stage': 'Final', 'dateTimeCet': '2021-02-06T20:00:00', 'watchLinks': [{'link': 'https://nrk.no/mgp', 'comment': 'Recommended link', 'live': 1},
+            {'country': 'Norway', 'name': 'Melodi Grand Prix', 'stage': 'Final (part 2)', 'dateTimeCet': '2021-02-06T21:20:00', 'watchLinks': [{'link': 'https://nrk.no/mgp', 'comment': 'Recommended link', 'live': 1},
         ]
         thread = self.generator.generate_thread(events)
         self.assertEqual(len(thread), 1)
