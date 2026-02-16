56cbd727d4dcec6e5ec104b5aed0650eb5ebf32d
corentindautreme <co@Cos.localdomain>
2026-02-16 21:35:17 +0100

    Add Threads formatter to encode URLs

--- publisher/threads_daily_publisher.py
+++ publisher/threads_daily_publisher.py
@@ -1,10 +1,11 @@
 from publisher.publisher import Publisher
 from client.threads_client import ThreadsClient
 from generator.daily_generator import DailyGenerator
+from formatter.threads_formatter import ThreadsFormatter
 
 class ThreadsDailyPublisher(Publisher):
     def __init__(self):
-        super().__init__(ThreadsClient(), DailyGenerator())
+        super().__init__(ThreadsClient(), DailyGenerator(formatter=ThreadsFormatter()))
 
 
     def get_log_header(self):
--- publisher/threads_weekly_publisher.py
+++ publisher/threads_weekly_publisher.py
@@ -1,10 +1,11 @@
 from publisher.publisher import Publisher
 from client.threads_client import ThreadsClient
 from generator.weekly_generator import WeeklyGenerator
+from formatter.threads_formatter import ThreadsFormatter
 
 class ThreadsWeeklyPublisher(Publisher):
     def __init__(self):
-        super().__init__(ThreadsClient(), WeeklyGenerator())
+        super().__init__(ThreadsClient(), WeeklyGenerator(formatter=ThreadsFormatter()))
 
 
     def get_log_header(self):
